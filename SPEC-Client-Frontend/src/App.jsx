423065e2b0cb0982813ee9b781ab7f261267e12c
amitdervadiya <amitdervadiya@gmail.com>
2025-12-01 18:59:59 +0530

    add

--- SPEC-Client-Frontend/src/App.jsx
+++ SPEC-Client-Frontend/src/App.jsx
@@ -1,5 +1,5 @@
  import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Homepage from "./routes/homepage";
+import Homepage from "./routes/Homepage";
 import category from "./routes/category";
 
 
