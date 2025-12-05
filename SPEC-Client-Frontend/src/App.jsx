332125c625743a92eec49e590c818e1eaa10ef76
amitdervadiya <amitdervadiya@gmail.com>
2025-12-05 22:55:02 +0530

    add

--- SPEC-Client-Frontend/src/App.jsx
+++ SPEC-Client-Frontend/src/App.jsx
@@ -1,23 +1,14 @@
- import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Homepage from "./routes/Homepage"
-import Category from "./routes/CategoryPage";
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./routes/Homepage";
+import CategoryPage from "./routes/CategoryPage";
 
 export default function App() {
   return (
-   
-   
-   <>
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" Component={Homepage}></Route>
-    <Route path="/categories" Component={Category}></Route>
-   </Routes>
-   </BrowserRouter>
-   </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/categories" element={<CategoryPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-  
