786029c8ba4c893c5855fd0366a6fe129f96d0b2
amitdervadiya <amitdervadiya@gmail.com>
2025-12-10 19:12:19 +0530

    add

--- SPEC-Client-Frontend/src/contactcom/Contacts.jsx
+++ SPEC-Client-Frontend/src/contactcom/Contacts.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+
 import rightarrow from "../assets/images/ArrowRight(1).svg"
 import map from "../assets/images/fmapicon.svg"
 import phone from "../assets/images/fphoneicon.svg"
 import email from "../assets/images/femailicon.svg"
 
 export default function Contacts() {
+  const contactData = [
+  {
+    id: 1,
+    title: "Office Address",
+    icon: map,
+    content: (
+      <p className="font-medium text-[16px] p-text">
+        sed no.12, Gondal Rd, near solvant fatak, chokdi, Rajkot, Kotharia, Gujarat 360022
+      </p>
+    ),
+  },
+  {
+    id: 2,
+    title: "Sales Reservation Team",
+    icon: phone,
+    content: (
+      <div className="w-full pr-4 h-full p-text">
+        <a href="tel:+919016699400" className="w-full flex justify-between a-contact">
+          <span>Rajkot Office:</span> 
+          <span>+91 90166 99400</span>
+        </a>
+        <a href="tel:+919879368602" className="w-full flex justify-between a-contact">
+          <span>At Resort:</span>
+          <span>+91 98793 68602</span>
+        </a>
+      </div>
+    ),
+  },
+  {
+    id: 3,
+    title: "Office Email",
+    icon: email,
+    content: (
+      <p className="font-medium text-[16px] p-text">spec@infospecco.in</p>
+    ),
+  },
+];
   return (
     <>
       <section className='flex justify-center items-center mt-[140px] max-md:h-max'>
@@ -20,7 +57,7 @@ export default function Contacts() {
               <h1 className='text-[50px] font-bold leading-none'>Enquire Now</h1>
               <p className='text-[18px] mt-[30px] text-[var(--grey)] max-sm:text-center'>We’re here to assist you with any questions about your orders, products, or services. Please fill out the form below and our support team will respond shortly.</p>
               <div className='flex flex-col w-full mt-[30px] gap-5'>
-                <div className='flex w-full justify-between gap-[25px] max-sm:flex-col'>
+                <div className='flex w-full justify-between gap-[25px] max-sm:flex-col input-res'>
                   <div className='flex-col flex gap-2 w-full'>
                     <label htmlFor="" className='text-[16px] font-semibold'>First Name</label>
                     <input type="text" className='input1 ' placeholder='Jone' />
@@ -52,57 +89,58 @@ export default function Contacts() {
                     Send Message  <img src={rightarrow} alt="" className='w-5 h-5 max-sm:h-[17px] max-sm:w-[17px]' />
                   </button>
                 </a>
-              </div>
+              </div>  
 
             </div>
 
           </div>
         </div>
       </section>
-      <section className='flex justify-center items-center mt-[140px] max-sm:mt-[50px] '>
-        <div className='w-[85%] flex justify-center max-sm:flex-col items-center max-sm:h-max h-[175px]'>
-          <div className='flex justify-center w-[30%] max-sm:w-full  h-[175px] gap-2 items-center p-[25px] border border-[#E4E4E4]'>
-            <div className='flex justify-center items-start h-full'>
-              <div className='h-[45px]  flex justify-center items-center w-[45px] rounded-full bg-[var(--brown)]'>
-                <img src={map} alt="" className='h-6 w-6' />
+
+   
+       <section className="flex justify-center items-center mt-[140px] max-sm:mt-[50px]">
+      <div
+        className="
+          w-[85%] 
+          grid 
+          grid-cols-1
+          md:grid-cols-2
+          lg:grid-cols-3
+          gap-5
+          con-box
+        "
+      >
+        {contactData.map((item) => (
+          <div
+            key={item.id}
+            className="
+              flex 
+              gap-2 
+              items-center 
+              p-[25px] 
+              h-[175px]
+
+              border border-[#E4E4E4]
+              box-c
+            "
+          >
+            <div className="flex justify-center items-start h-full">
+              <div className="h-[45px] w-[45px] flex justify-center items-center rounded-full bg-[F9F5EF]">
+                <img src={item.icon} alt="" className="h-6 w-6" />
               </div>
             </div>
-            <div className='flex flex-col gap-2'>
-              <h6 className='text-[var(--black)] text-[22px]  max-sm:text-[18px] font-semibold'>Office Address</h6>
-              <p className='font-medium text-[16px]'>sed no.12, Gondal Rd, near solvant fatak, chokdi, Rajkot, Kotharia, Gujarat 360022</p>
+
+            <div className="flex flex-col gap-2 justify-start h-full">
+              <h6 className="text-[var(--black)] text-[22px] font-semibold max-sm:text-[18px] h6">
+                {item.title}
+              </h6>
+
+              {item.content}
             </div>
           </div>
-          <div className='flex justify-center gap-2 w-[30%]   max-sm:w-full   h-[175px] items-center p-[25px] border border-[#E4E4E4]'>
-            <div className='flex justify-center items-start h-full'>
-              <div className='h-[45px]  flex justify-center items-center w-[45px] rounded-full bg-[var(--brown)]'>
-                <img src={phone} alt="" className='h-6 w-6' />
-              </div>
-            </div>
-            <div className='flex flex-col  justify-center h-full gap-2 w-full'>
-              <h6 className='text-[var(--black)] text-[22px] font-semibold max-sm:text-[18px]'>Sales Reservation Team</h6>
-              <div className='w-full pr-4 h-full'>
-                <a href="tel:+919016699400" className='w-full flex justify-between'><span>Rajkot Office:</span> <span>+91 90166 99400</span></a>
-                <a href="tel:+919879368602" className='w-full flex justify-between'> <span>At Resort:</span><span>+91 98793 68602</span></a>
-              </div>
-            </div>
-          </div>
-          <div className='flex justify-center gap-2 items-center max-sm:justify-start  h-[175px] w-[30%]  max-sm:w-full  p-[25px] border border-[#E4E4E4]'>
-            <div className='flex justify-center items-star h-full'>
-              <div className='h-[45px]  flex justify-center items-center w-[45px] rounded-full bg-[var(--brown)]'>
-                <img src={email} alt="" className='h-6 w-6' />
-              </div>
-            </div>
-            <div className='flex flex-col gap-2 justify-start h-full'>
-              <h6 className='text-[var(--black)] text-[22px] font-semibold max-sm:text-[18px]'>Office Email</h6>
-              <p className='font-medium text-[16px]'>spec@infospecco.in</p>
-            </div>
-          </div>
-
-
-
-
-        </div>
-      </section>
+        ))}
+      </div>
+    </section>
     </>
   )
 }
---
+++ SPEC-Client-Frontend/src/productcom/Producthead.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Producthead() {
+    return (
+        <>
+         <section>
+            <div className="w-[85%]">
+                <div>
+                     
+                </div>
+            </div>
+         </section>
+        </>
+    )
+}
